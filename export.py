2d499cf27645d3ef9f670a3f6d45de69528be3f0
Renne Ong <renneong26@gmail.com>
2025-06-18 21:08:48 +0800

    Export vocab to json

--- export.py
+++ export.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import os
+import json
 import onnx
 import onnxruntime
 import numpy as np
@@ -48,8 +49,10 @@ class Model(nn.Module):
     return decoded_ids
 
 def exportModel(encoder, decoder, input_vocab, output_vocab, max_length, eos_token, unk_token, testLoader):
-  if not os.path.exists(f'./Models/'):
-    os.makedirs(f'./Models/')
+  if not os.path.exists(f'./Exports/'):
+    os.makedirs(f'./Exports/')
+    os.makedirs(f'./Exports/Models/')
+    os.makedirs(f'./Exports/Vocab/')
   
   pair = testLoader.dataset[np.random.randint(0, len(testLoader.dataset))]
   example_inputs = pair[0].cpu()
@@ -59,10 +62,34 @@ def exportModel(encoder, decoder, input_vocab, output_vocab, max_length, eos_tok
   onnx_program = torch.onnx.export(model, example_inputs, input_names=['Input'], dynamo=True, opset_version=21)
   onnx_program.optimize()
 
-  onnx_program.save('./Models/en_jp_translation_model.onnx')
+  model_export_dir = './Exports/Models/en_jp_translation_model.onnx'
+  onnx_program.save(model_export_dir)
+
+  print('Exporting vocab')
+  input_vocab_json = {
+    'name': input_vocab.name,
+    'word2index': input_vocab.word2index,
+    'word2count': input_vocab.word2count,
+    'index2word': input_vocab.index2word,
+    'n_words': input_vocab.n_words
+  }
+
+  output_vocab_json = {
+    'name': output_vocab.name,
+    'word2index': output_vocab.word2index,
+    'word2count': output_vocab.word2count,
+    'index2word': output_vocab.index2word,
+    'n_words': output_vocab.n_words
+  }
+
+  with open('./Exports/Vocab/input_vocab.json', 'w') as file:
+    json.dump(input_vocab_json, file)
+  
+  with open('./Exports/Vocab/output_vocab.json', 'w') as file:
+    json.dump(output_vocab_json, file)
 
   print('Loading model to validate predictions')
-  onnx_model = onnx.load('./Models/en_jp_translation_model.onnx')
+  onnx_model = onnx.load(model_export_dir)
   onnx.checker.check_model(onnx_model)
 
   decoded_onnx_input, _ = decodePairs(pair[0], pair[1], input_vocab, output_vocab, unk_token, eos_token)
@@ -70,7 +97,7 @@ def exportModel(encoder, decoder, input_vocab, output_vocab, max_length, eos_tok
   print(f'Input length: {len(pair[0])}')
   print(f'Sample input: {decoded_onnx_input}')
 
-  ort_session = onnxruntime.InferenceSession('./Models/en_jp_translation_model.onnx', providers=['CPUExecutionProvider'])
+  ort_session = onnxruntime.InferenceSession(model_export_dir, providers=['CPUExecutionProvider'])
   onnx_input = {'Input': pair[0].cpu().numpy()}
   onnx_outputs = ort_session.run(None, onnx_input)[0]
   torch_outputs = model(pair[0].cpu())
